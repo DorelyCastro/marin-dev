561800ea0c563114e4222c10f98eeb2e3413af2f
2021IteSoluciones <86262454+2021IteSoluciones@users.noreply.github.com>
2021-07-21 02:25:08 -0500

    add_report_batch_min
    add_report_batch_min

---
+++ informe_batch_min/models/informe_batch_min.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from odoo import models, fields, api
+
+
+class informeBatchm(models.Model):
+    _name = 'informe.batchmin'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char('Nombre', default='Informe Batch minimo')
+    detalle_ids = fields.One2many('informe.batchmin.detalle', 'informe_bm_id', string="Detalles")
+    ultima_actualizacion = fields.Datetime('Ultima actualización')
+
+    def calcular(self):
+        self.ultima_actualizacion = datetime.datetime.now()
+        for detalle in self.detalle_ids:
+            detalle.unlink()
+        sale_ids = self.env['stock.move'].search([
+            ('route_ids', 'ilike', 'Fabri'),
+            ('location_id', 'not ilike', 'virtual'),
+            ('picking_type_id', 'ilike', 'Manufac'),
+            ('state', 'in', ['confirmed', 'partially_available']),
+        ])
+        product_ids = sale_ids.mapped('product_id')
+        for product_id in product_ids:
+            sale_filtrada_ids = sale_ids.filtered(lambda x: x.product_id == product_id)
+            product_quantity_ids = self.env['mrp.bom'].search([
+                ('product_tmpl_id', '=', product_id.product_tmpl_id.id),
+            ])
+            self.env['informe.batchmin.detalle'].create({
+                'informe_bm_id': self.id,
+                'default_code': product_id.default_code,
+                'product_id': product_id.id,
+                'product_name': product_id.display_name,
+                'product_qty': sum(sale_filtrada_ids.mapped('product_uom_qty')),
+                'cantidad_reservada': sum(sale_filtrada_ids.mapped('reserved_availability')),
+                'batch_minimo': sum(product_quantity_ids.mapped('product_qty')),
+            })
+
+class batchminDetalle(models.Model):
+    _name = 'informe.batchmin.detalle'
+
+    informe_bm_id = fields.Many2one('informe.batchmin', string='Informe batch minimo')
+    product_id = fields.Many2one('product.product', string='Producto')
+    product_qty = fields.Float('Cantidad demandada')
+    cantidad_reservada = fields.Float('Cantidad reservada')
+    cantidad_real = fields.Float('Cantidad real', store=True, compute='_compute_cantidad_real')
+    diferencia = fields.Float('Diferencia', store=True, compute='_compute_diferencia')
+    batch_minimo = fields.Float('Batch minimo')
+    product_name = fields.Char('Nombre producto')
+    default_code = fields.Char('Codigo')
+
+    @api.depends('product_qty', 'cantidad_reservada')
+    def _compute_cantidad_real(self):
+        for record in self:
+            if record.product_qty and record.cantidad_reservada:
+                record.cantidad_real = record.product_qty - record.cantidad_reservada
+            else:
+                record.cantidad_real = 0.0
+
+    @api.depends('cantidad_real', 'batch_minimo')
+    def _compute_diferencia(self):
+        for record in self:
+            if record.cantidad_real and record.batch_minimo:
+                record.diferencia = record.batch_minimo - record.cantidad_real
+            else:
+                record.diferencia = 0.0
+
+class bomProduct(models.Model):
+    _inherit = 'stock.move'
+
+    route_ids = fields.Many2many('stock.location.route', string='Rutas', related='product_id.route_ids')
